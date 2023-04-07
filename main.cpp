c4246d5640044291d5a1bc5d994304ca8184ff46
voozen <voozen3@gmail.com>
2023-04-07 14:45:56 +0200

    add newton-raphson method

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 
-#define eps 0.001
+#define eps 0.0001
 
 double func(double x) {
-    return x*x*x - x*x + 2;
+    return x * x * x - x * x + 2;
+}
+
+double deriv(double x) {
+    return 3 * x * x - 2 * x;
 }
 
 void bisection(double a, double b) {
     double x;
-    while (b - a > eps) {
+    while (std::abs(b - a) > eps) {
         x = (a + b) / 2;
         if (func(x) == 0) return;
         else {
@@ -20,7 +24,17 @@ void bisection(double a, double b) {
     std::cout << x << std::endl;
 }
 
+void newtonRaphson(double x) {
+    double g = func(x) / deriv(x);
+    while (std::abs(g) >= eps) {
+        g = func(x) / deriv(x);
+        x -= g;
+    }
+    std::cout << x << std::endl;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     bisection(-100, 100);
+    newtonRaphson(-10);
 }
